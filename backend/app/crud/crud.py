ba9478eb82beae03ec0029da042164e8c2401f81
Oisin Dermody <oisindermody@gmail.com>
2025-02-07 21:40:15 +1100

    refactor: Simplifying CRUD operations - logic handled at respective router instead

--- backend/app/crud/crud.py
+++ backend/app/crud/crud.py
@@ -3,23 +3,24 @@ import shutil
 from fastapi import HTTPException, UploadFile, File, Depends
 from fastapi_cli.cli import app
 from sqlmodel import select, Session
-from backend.app.main import get_session
+
+from backend.app.api import SessionDep
 from backend.app.models import NarrativeComponent, Narrative
 
 @app.get("/components", response_model=list[NarrativeComponent])
-def read_components(session: Session = Depends(get_session)):
+def read_components(session: SessionDep):
     statement = select(NarrativeComponent)
     return session.exec(statement).all()
 
 
 @app.get("/components/{component_id}", response_model=NarrativeComponent)
-def read_component(component_id: int, session: Session = Depends(get_session)):
+def read_component(component_id: int, session: SessionDep):
     statement = select(NarrativeComponent).where(NarrativeComponent.id == component_id)
     return session.exec(statement).first()
 
 
 @app.post("/components", response_model=NarrativeComponent)
-def create_component(component: NarrativeComponent, session: Session = Depends(get_session)):
+def create_component(component: NarrativeComponent, session: SessionDep):
     try:
         session.add(component)
         session.commit()
@@ -31,7 +32,7 @@ def create_component(component: NarrativeComponent, session: Session = Depends(g
 
 
 @app.put("/components/{component_id}", response_model=NarrativeComponent)
-def update_component(component_id: int, updated_component: NarrativeComponent, session: Session = Depends(get_session)):
+def update_component(component_id: int, updated_component: NarrativeComponent, session: SessionDep):
     try:
         statement = select(NarrativeComponent).where(NarrativeComponent.id == component_id)
         component = session.exec(statement).first()
@@ -49,7 +50,7 @@ def update_component(component_id: int, updated_component: NarrativeComponent, s
 
 
 @app.delete("/components/{component_id}")
-def delete_component(component_id: int, session: Session = Depends(get_session)):
+def delete_component(component_id: int, session: SessionDep):
     try:
         session.delete(component_id)
         session.commit()
@@ -60,32 +61,25 @@ def delete_component(component_id: int, session: Session = Depends(get_session))
         raise HTTPException(status_code=500, detail=f"Failed to delete component: {str(e)}")
 
 
-@app.get("/narratives", response_model=list[Narrative])
-def read_narratives(session: Session = Depends(get_session)):
+def get_narratives(session: SessionDep):
     statement = select(Narrative)
     return session.exec(statement).all()
 
 
-@app.get("/narratives/{narrative_id}", response_model=Narrative)
-def read_narrative(narrative_id: int, session: Session = Depends(get_session)):
+def get_narrative_by_id(narrative_id: int, session: SessionDep):
     statement = select(Narrative).where(Narrative.id == narrative_id)
     return session.exec(statement).first()
 
 
-@app.post("/narratives", response_model=Narrative)
-def create_narrative(narrative: Narrative, session: Session = Depends(get_session)):
-    try:
-        session.add(narrative)
-        session.commit()
-        session.refresh(narrative)
-        return narrative
-    except Exception as e:
-        session.rollback()
-        raise HTTPException(status_code=500, detail=f"Failed to insert narrative: {str(e)}")
+def create_narrative(narrative: Narrative, session: SessionDep):
+    session.add(narrative)
+    session.commit()
+    session.refresh(narrative)
+    return narrative
 
 
 @app.put("/narratives/{narrative_id}", response_model=Narrative)
-def update_narrative(narrative_id: int, updated_narrative: Narrative, session: Session = Depends(get_session)):
+def update_narrative(narrative_id: int, updated_narrative: Narrative, session: SessionDep):
     try:
         statement = select(Narrative).where(Narrative.id == narrative_id)
         narrative = session.exec(statement).first()
@@ -102,16 +96,11 @@ def update_narrative(narrative_id: int, updated_narrative: Narrative, session: S
         raise HTTPException(status_code=500, detail=f"Failed to update narrative: {str(e)}")
 
 
-@app.delete("/narratives/{narrative_id}")
-def delete_narrative(narrative_id: int, session: Session = Depends(get_session)):
-    try:
-        session.delete(narrative_id)
-        session.commit()
-        session.refresh(narrative_id)
-        return {"ok - component deleted successfully": True}
-    except Exception as e:
-        session.rollback()
-        raise HTTPException(status_code=500, detail=f"Failed to delete narrative: {str(e)}")
+def delete_narrative(narrative_id: int, session: SessionDep):
+    session.delete(narrative_id)
+    session.commit()
+    session.refresh(narrative_id)
+    return Narrative
 
 
 # Rudimentary file upload method for initial development using Swagger UI docs
