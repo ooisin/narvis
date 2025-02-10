5306d0f4da5dece25139a1145c0b2a7f31b4318c
Oisin Dermody <oisindermody@gmail.com>
2025-02-10 15:57:31 +1100

    feat: Create superuser/admin on startup if None

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,10 +1,20 @@
 from sqlmodel import Session, create_engine, select, SQLModel
 
 from backend.app.core.config import settings
+from backend.app.crud import crud
+from backend.app.models.models import User, UserCreate
 
 engine = create_engine(str(settings.DATABASE_URI), echo=True)
 
 
 def init_db(session: Session) -> None:
     SQLModel.metadata.create_all(engine)
-    # TODO: Insert all user logic checks - create if not in db
+
+    user = session.exec(select(User).where(User.email == settings.FIRST_SUPERUSER)).first()
+    if not user:
+        user_in = UserCreate(
+            email=settings.FIRST_SUPERUSER,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_superuser=True,
+        )
+        user = crud.create_user(session=session, user_create=user_in)
