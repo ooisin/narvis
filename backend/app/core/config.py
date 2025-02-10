76e953ff6a9f816493e66779183057cc4fee9295
Oisin Dermody <oisindermody@gmail.com>
2025-02-10 15:23:37 +1100

    feat: Token generation and hashing functions implemented

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,3 +1,5 @@
+import secrets
+
 from pydantic import computed_field, ValidationError
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic_core import MultiHostUrl
@@ -9,6 +11,11 @@ class Settings(BaseSettings):
         env_ignore_empty=True,
         extra="ignore"
     )
+
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    HASHING_ALGORITHM: str = "HS256" # TODO: Should this be abstracted?
+    ACCESS_TOKEN_EXPIRE_DAYS: int = 7
+    FRONTEND_HOST: str = "http://localhost:5173"
 
     PROJECT_NAME: str
     POSTGRES_SERVER: str
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -4,14 +4,18 @@ from typing import Any
 from backend.app.core.config import settings
 from datetime import datetime, timedelta, timezone
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def generate_token(entity: str | Any, expires_delta: timedelta) -> str:
-    pass
+def generate_token(entity: str | Any, alive_time: timedelta) -> str:
+    expire_time = datetime.now(timezone.utc) + alive_time
+    encode_entity = {"exp": expire_time, "sub": str(entity)}
+    encoded_token = jwt.encode(encode_entity, settings.SECRET_KEY, algorithm=settings.HASHING_ALGORITHM)
+    return encoded_token
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    pass
+    return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password: str) -> str:
-    pass
+    return pwd_context.hash(password)
