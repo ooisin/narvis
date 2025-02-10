f0952560213ea0f19f6d4e21d9f2e76c2cf9c1bf
Oisin Dermody <oisindermody@gmail.com>
2025-02-10 20:00:54 +1100

    feat: User status dependency checks and Token dependency

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,10 +1,16 @@
 from typing import Annotated
 from collections.abc import Generator
-
-from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException
+from jwt import InvalidTokenError
+from pydantic import ValidationError
 from sqlmodel import Session
+import jwt
+from backend.app.core import engine, settings
+from backend.app.models.models import User, TokenPayload
 
-from backend.app.core import engine
+
+oauth2_extract = OAuth2PasswordBearer(tokenUrl="/login/access-token")
 
 
 def get_db() -> Generator[Session, None, None]:
@@ -13,3 +19,27 @@ def get_db() -> Generator[Session, None, None]:
 
 
 SessionDep = Annotated[Session, Depends(get_db)]
+TokenDep = Annotated[str, Depends(oauth2_extract)]
+
+
+def get_current_user(session: SessionDep, token: TokenDep) -> User:
+    try:
+        data = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.HASHING_ALGORITHM])
+        token_data = TokenPayload(**data)
+    except (InvalidTokenError, ValidationError):
+        raise HTTPException(status_code=403, detail="Invalid token - could not validate credentials")
+
+    user = session.get(User, token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user - can't be current!")
+
+    return user
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
+def get_current_active_superuser(current_user: CurrentUser) -> User:
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=403, detail="The user is not superuser")
+    return current_user
