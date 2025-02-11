cc85645e1c44995675cf4a29172888dce2a1b4bb
Oisin Dermody <oisindermody@gmail.com>
2025-02-11 23:50:21 +1100

    feat: Simple start script to initialise everything properly

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-python app/db_connection_check.py
-python app/initial_data.py
+python backend/app/db_connection_check.py
+python backend/app/initial_data.py
 
-uvicorn backend.app.main:app --host 0.0.0.0 --port 8000
+uvicorn backend.app.main:app --host 127.0.0.1 --port 8000 --reload
