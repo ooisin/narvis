0261e2e0922b2f367b635abfa77698abbda2fe75
Oisin Dermody <oisindermody@gmail.com>
2025-02-20 20:59:49 +1100

    refactor: Updated types

--- backend/app/api/routes/narratives.py
+++ backend/app/api/routes/narratives.py
@@ -11,9 +11,11 @@ router = APIRouter(prefix="/narratives", tags=["narratives"])
 
 
 @router.get("/", response_model=NarrativesPublic)
-def read_narratives(session: SessionDep, current_user: CurrentUser, limit: int = 100) -> Any:
+def get_narratives(session: SessionDep, current_user: CurrentUser, limit: int = 100) -> Any:
 
     if current_user.is_superuser:
+        # TODO: Abstract this logic to a crud method!
+        # Needs error handling etc
         count_statement = select(func.count()).select_from(Narrative)
         count = session.exec(count_statement).one()
         statement = select(Narrative).limit(limit)
@@ -36,23 +38,35 @@ def read_narratives(session: SessionDep, current_user: CurrentUser, limit: int =
 
 
 @router.get("/{id}", response_model=NarrativePublic)
-def read_narrative(session: SessionDep, current_user: CurrentUser, id: uuid.UUID) -> Any:
+def get_narrative(session: SessionDep, current_user: CurrentUser, id: uuid.UUID) -> Any:
 
-    item = session.get(Narrative, id)
-    if not item:
+    narrative = session.get(Narrative, id)
+    if not narrative:
         raise HTTPException(status_code=404, detail=f"Narrative with id {id} not found")
-    if not current_user.is_superuser and (item.owner_id != current_user.id):
+    if not current_user.is_superuser and (narrative.owner_id != current_user.id):
         raise HTTPException(status_code=400, detail="No permission to access this narrative")
-    return item
+    return narrative
 
 
 @router.post("/", response_model=NarrativePublic)
-def create_item(*, session: SessionDep, current_user: CurrentUser, narrative_in: NarrativeCreate) -> Any:
+def create_narrative(session: SessionDep, current_user: CurrentUser, narrative_in: NarrativeCreate) -> Any:
     narrative = Narrative.model_validate(narrative_in, update={"owner_id": current_user.id})
     session.add(narrative)
     session.commit()
     session.refresh(narrative)
     return narrative
+
+
+@router.delete("/{id}")
+def delete_narrative(session: SessionDep, current_user: CurrentUser, id: uuid.UUID) -> str:
+    narrative = session.get(Narrative, id)
+    if not narrative:
+        raise HTTPException(status_code=404, detail=f"Narrative with id {id} not found")
+    if not current_user.is_superuser and (narrative.owner_id != current_user.id):
+        raise HTTPException(status_code=400, detail="No permission to delete this narrative")
+    session.delete(narrative)
+    session.commit()
+    return f"Narrative: {id} deleted successfully"
 
 
 
